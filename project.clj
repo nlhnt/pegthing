7e22fdedaef8f624abb5c41c7e1775d2dfdbec5d
nlhnt <grimonce@gmail.com>
2023-12-04 23:09:48 +0100

    update to clojure 1.11 calva and clj-kondo

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot pegthing.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all
+                     :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
